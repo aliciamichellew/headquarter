769eaddc81c18e6e51e36aab46689a19a912b84f
Alicia <aliciamichellew@gmail.com>
2022-07-25 21:01:59 +0800

    fix message model bug

--- server/models/MessageModel.js
+++ server/models/MessageModel.js
@@ -1,27 +1,27 @@
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema(
-    {
-        sender:{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "users",
-            required: true,
-      },
-
-        message: {
-            type: String,
-            required: true,
-        },
-
-        chat: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "chats",
-            required: true,
-        },
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+
+    message: {
+      type: String,
+      required: true,
+    },
+
+    chat: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "chats",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const message = mongoose.model("messages", messageSchema);
