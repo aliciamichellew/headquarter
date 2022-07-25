b7863720fbecc30bff80275710e2cb7f6c5e45a6
Alicia <aliciamichellew@gmail.com>
2022-07-25 21:21:44 +0800

    change port

--- server/server.js
+++ server/server.js
@@ -20,9 +20,35 @@ const app = express();
 
 dotenv.config();
 
+const port = process.env.PORT || 4000;
+
+const server = app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    limit: "50mb",
+  })
+);
+
 mongoose.connect(process.env.DATABASE_ACCESS, () =>
   console.log("Database connected")
 );
+
+app.use(cors());
+app.use("/api/internships", internshipRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/modules", moduleRoutes);
+app.use("/api/profile", profileRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/message", messageRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -45,44 +71,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // app.use(express.json());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-    limit: "50mb",
-  })
-);
-// for parsing json
-// app.use(
-//   bodyParser.json({
-//     limit: "20mb",
-//   })
-// );
-
-// // for parsing application/x-www-form-urlencoded
-// app.use(
-//   bodyParser.urlencoded({
-//     limit: "20mb",
-//     extended: true,
-//   })
-// );
-app.use(cors());
-app.use("/api/internships", internshipRoutes);
-app.use("/api/chat", chatRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/modules", moduleRoutes);
-app.use("/api/profile", profileRoutes);
-app.use("/api/post", postRoutes);
-app.use("/api/message", messageRoutes);
-app.use(notFound);
-app.use(errorHandler);
-
-const port = 4000;
-
-const server = app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
 
 app.post("/", async (req, res) => {});
 
